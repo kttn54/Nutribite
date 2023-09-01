898331bd7a30e65e516ccde6b486bc2bb138512e
stporg <127300104+stporg@users.noreply.github.com>
2023-09-01 11:44:51 +1000

    Update camera fragment

--- app/src/main/java/com/example/sc_nutri/fragments/CameraFragment.kt
+++ app/src/main/java/com/example/sc_nutri/fragments/CameraFragment.kt
@@ -1,13 +1,21 @@
 package com.example.sc_nutri.fragments
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.sc_nutri.databinding.FragmentCameraBinding
 
 class CameraFragment: Fragment() {
+
+    companion object {
+        private const val REQUEST_CAMERA_CODE = 1
+    }
 
     private lateinit var binding: FragmentCameraBinding
 
@@ -22,7 +30,13 @@ class CameraFragment: Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_CODE)
+        }
 
+        binding.btnCapture.setOnClickListener {
+
+        }
     }
 
 }
